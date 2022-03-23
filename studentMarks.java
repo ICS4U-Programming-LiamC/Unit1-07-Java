386c5bba89e09102000d7a8646d3e0e6aa93c9b2
Liam Csiffary <liam.csiffary@stu.ocsb.ca>
2022-03-23 09:31:26 -0400

    finished studentMarks, style checking and comments done

--- studentMarks.java
+++ studentMarks.java
@@ -3,24 +3,55 @@
 * marks, geberated with standard deviation
 *
 * @author  Liam Csiffary
-* @version 1.0
+* @version 1.1
 * @since   2022-03-22
 */
 
+// import required modules
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
+import java.util.Random;
 
+// class
 public class studentMarks {
-  public static String[] Mode(String[] arrayOfStudents, String[] arrayOfAssignments) {
-    String[] finalArray = new String[arrayOfStudents.length];
+
+  // Generates the random marks and populates the array of arrays with them
+  public static String[][] StudentMarks(String[] arrayOfStudents, String[] arrayOfAssignments) {
+    // create the random object
+    Random r = new Random();
+
+    // create the array of type array of type string
+    String[][] finalArray = new String[arrayOfStudents.length][arrayOfAssignments.length + 1];
+
+    // for each student ...
+    for (int studentName = 0; studentName < arrayOfStudents.length; studentName++) {
+      // should reset the array so it can be refiled
+      String[] tempArray = new String[arrayOfAssignments.length + 1];
+      
+      // puts name in the first slot
+      tempArray[0] = arrayOfStudents[studentName];
+
+      // for each in assignments ...
+      for (int mark = 0; mark < arrayOfAssignments.length; mark++) {
+        // pupulate inner array skipping the first index
+        // which contains the students name
+        tempArray[mark + 1] = Double.toString(r.nextGaussian() * 10 + 75);
+      }
+
+      // add the generated array to the final array
+      finalArray[studentName] = tempArray;
+    }
     return finalArray;
   }
 
 
-  public static String[] arrayGenerator(file file) {
+  // generates the arrays from the files
+  public static String[] ArrayGenerator(File file) throws Exception {
+
     // defining vars
     List<String> originalListString = new ArrayList<String>();
-
     System.out.println(file);
     Scanner scStudent = new Scanner(file);
 
@@ -32,8 +63,10 @@ public class studentMarks {
     // for testing purposes
     System.out.println(originalListString);
 
+    // create array of length
     String[] array = new String[originalListString.size()];
 
+    // fill the array
     for (int i = 0; i < array.length; i++) {
       array[i] = originalListString.get(i);
     }
@@ -41,21 +74,76 @@ public class studentMarks {
     return array;
   }
 
+  // code from https://www.w3schools.com/java/java_files_create.asp
+  // my comments to prove I know what is happening though
+  public static void CreateFile() {
+    try {
+      // creates the file name/object
+      File myObj = new File("StudentMarks.txt");
+
+      // if it is able to create a new file
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      
+      // otherwise states that the file already exists
+      } else {
+        System.out.println("File already exists.");
+      }
+    
+    // if exeception
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+    
+      // traces it I guess?
+      e.printStackTrace();
+    }
+  }
+
+  // also from https://www.w3schools.com/java/java_files_create.asp
+  public static void Writer(String[][] arr) {
+    try {
+      // creates file writing object
+      FileWriter myWriter = new FileWriter("StudentMarks.txt");
+
+      // my code
+      // just writes to the file the same way you'd print it to the terminal
+      // except with myWriter.write instead of System.out.print()
+      for (int each = 0; each < arr.length; each++) {
+        for (int index = 0; index < arr[each].length; index++) {
+          myWriter.write(arr[each][index] + ", ");
+        }
+        myWriter.write("\n");
+      }
+
+      // closes the file
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
 
   public static void main(String[] args) throws Exception {
+    // generates new file and creates the array using the ArrayGenerator
     File file = new File(
         "C:/Users/s299776/ICS4U/Unit1/Java/Unit1-07-Java/students.txt");
     
-    String[] studentArray = arrayGenerator(file);
+    String[] studentArray = ArrayGenerator(file);
 
-    file = File(
+    // repeats but for the marks
+    file = new File(
         "C:/Users/s299776/ICS4U/Unit1/Java/Unit1-07-Java/students.txt");
     
-    String[] assignArray = arrayGenerator(file);
+    String[] assignArray = ArrayGenerator(file);
 
-    finalArr = StudentMarks(studentArray, assignArray);
-    
+    // gets the final array from the StudentMarks function
+    String[][] finalArr = StudentMarks(studentArray, assignArray);
 
+    // creates and writes to a new file called "StudentMarks.txt"
+    CreateFile();
+    Writer(finalArr);
 
   }
 }
